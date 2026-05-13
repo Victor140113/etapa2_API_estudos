55a52681fbdd72a63ffa59061e4ae5fdbf465163
Victor Hugo <211873799+Victor140113@users.noreply.github.com>
2026-05-13 10:39:33 -0300

    feat: Criação da estrutura inicial do projeto

---
+++ src/main/java/com/api/etapa2_API/Etapa2ApiApplication.java
@@ -0,0 +1,13 @@
+package com.api.etapa2_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Etapa2ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Etapa2ApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/etapa2_API/Etapa2ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.etapa2_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Etapa2ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
