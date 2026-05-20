11798297fec552c4527411f3d5e11053d8011101
Victor Hugo <211873799+Victor140113@users.noreply.github.com>
2026-05-20 12:01:15 -0300

    feat: Implementação da funcionalidade de atualizar matrícula

--- src/main/java/com/api/etapa2_API/controller/MatriculaController.java
+++ src/main/java/com/api/etapa2_API/controller/MatriculaController.java
@@ -6,10 +6,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.api.etapa2_API.dto.request.MatriculaAlunoRequest;
+import com.api.etapa2_API.dto.request.MatriculaAtualizarRequest;
 import com.api.etapa2_API.dto.response.MatriculaCursoResponse;
 import com.api.etapa2_API.dto.response.MatriculaDefaultResponse;
 import com.api.etapa2_API.dto.response.MatriculaUsuarioResponse;
@@ -54,6 +56,16 @@ public class MatriculaController {
 		
 		return ResponseEntity.ok(matriculas);
 		
+	}
+	
+	// Atualizar progresso e status de uma matricula;
+	@PutMapping("/matricula/{id}")
+	public ResponseEntity<MatriculaDefaultResponse> atualizarMatricula(@PathVariable Long id, @RequestBody MatriculaAtualizarRequest dados){
+		
+		MatriculaDefaultResponse matricula = service.atualizarMatricula(id, dados);
+		if(matricula == null) return ResponseEntity.status(404).build();
+		
+		return ResponseEntity.ok(matricula);
 	}
 	
 }
